b9279d4efe98c97dae11969d44d0041380ea006e
Christer Miroy <chrstrzm.mry@gmail.com>
2022-11-13 22:40:58 +0800

    first commit

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -18,7 +18,9 @@ const createJob = async(req, res) => {
 const getAllJobs = async(req, res) => {
     const jobs = await Job.find({createdBy: req.user.userId})
 
-    res.status(StatusCodes.OK).json({
+    res
+    .status(StatusCodes.OK)
+    .json({
         jobs,
         totalJobs: jobs.length,
         numOfPages: 1
