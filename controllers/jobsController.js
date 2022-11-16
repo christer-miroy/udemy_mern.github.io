401567baed9f0865673a757651a01790442723a1
Christer Miroy <chrstrzm.mry@gmail.com>
2022-11-16 16:11:32 +0800

    edit and delete backend done

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,11 @@
 import Job from '../models/Job.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
+import {
+    BadRequestError,
+    NotFoundError,
+    UnAuthenticatedError,
+} from '../errors/index.js'
+import checkPermissions from '../utils/checkPermissions.js'
 
 const createJob = async(req, res) => {
     const { company, position } = req.body
@@ -27,12 +32,61 @@ const getAllJobs = async(req, res) => {
     })
 }
 
-const deleteJob = async(req, res) => {
-    res.send('delete job')
+const updateJob = async(req, res) => {
+    const { id: jobId } = req.params
+    const { company, position } = req.body
+    /*  const { company, position, jobLocation } = req.body (second approach, see notes below) */
+
+    if (!company || !position) {
+        throw new BadRequestError('Please provide all values.')
+    }
+
+    const job = await Job.findOne({_id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId} found!`)
+    }
+
+    /* check permissions */
+    checkPermissions(req.user, job.createdBy)
+
+    /* first approach: will not trigger the hook */
+    const updatedJob = await Job.findByIdAndUpdate({_id: jobId}, req.body, {
+        new: true,
+        runValidators: true //will only run based on what is provided by req.body (enum values in this case)
+    })
+
+    res.status(StatusCodes.OK).json({ updatedJob })
+    
+
+    /* second approach - will also trigger the hook. cons: make sure all properties exist 
+    job.position = position
+    job.company = company
+    job.jobLocation = jobLocation   
+
+    await job.save()
+
+    res.status(StatusCodes.OK).json({ job })
+    */
 }
 
-const updateJob = async(req, res) => {
-    res.send('update job')
+const deleteJob = async(req, res) => {
+    const { id: jobId } = req.params
+
+    const job = await Job.findOne({_id: jobId })
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId} found!`)
+    }
+
+    /* check permissions */
+    checkPermissions(req.user, job.createdBy)
+
+    await job.remove()
+
+    res.status(StatusCodes.OK).json({
+        msg: 'Job removed successfully!'
+    })
 }
 
 const showStats = async(req, res) => {
